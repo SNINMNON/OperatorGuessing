e0d39b18f946f409bd81caf3df7aa5b80f6baa75
sninmnon <15313003952@163.com>
2025-07-03 12:37:56 +0800

    to be tested with db

--- src/main/java/com/sninmnon/demo/mapper/OpMapper.java
+++ src/main/java/com/sninmnon/demo/mapper/OpMapper.java
@@ -4,6 +4,7 @@ import com.sninmnon.demo.entity.Operator;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
+
 @Mapper
 public interface OpMapper {
     @Select("SELECT * FROM ops WHERE name = #{name}")
